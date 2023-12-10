00b2cdcdd3f8d043fd34fd99fed434c9a18d3e28
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2023-12-10 09:40:24 -0500

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ db.authenticate()
 
 initModels();
 
-db.sync({ force: false, alter: true })
+db.sync({ force: true })
   .then(() => console.log('Synchronized database'))
   .catch(error => console.log(error));
 
