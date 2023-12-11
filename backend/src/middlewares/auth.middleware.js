b5bded1f3959726304cc5de91e4f18de40ece51b
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2023-12-10 20:08:04 -0500

    update auth.middleware.js

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -6,6 +6,7 @@ const authenticate = (req, res, next) => {
   if (bearerToken) {
     const token = bearerToken.split("Bearer ")[1];
     try {
+      console.log(token)
       const decoded = jwt.verify(token, process.env.SECRET_KEY, "HS512");
       next();
     } catch (error) {
