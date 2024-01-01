743d2c4ed1448a349d4c77966fd74ba122e55299
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2024-01-01 09:31:35 -0500

    loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import ProtectedRoutes from './components/ProtectedRoutes.jsx';
@@ -11,16 +12,34 @@ import ListProsucts from './pages/ListProducts.jsx';
 import ReadQr from './components/ReadQr.jsx';
 import exitIcon from './assets/logout.svg';
 import Loader from './components/Loader.jsx';
+import axios from 'axios'
+import apiUrl from './utils/apiUrl.js'
 
 function App() {
+  const [showLoader, setShowLoader] = useState(true)
   const logout = () => {
     localStorage.clear();
     location.reload();
   };
 
+  const hiddeLoader = () => {
+    axios.get(`${apiUrl}/users/business/1`)
+      .then(res => {
+        setTimeout(() => {
+          setShowLoader(false)
+        }, 2000)
+      })
+      .catch(err => {
+        console.log(err)
+        setShowLoader(true)
+      })
+  }
+
+  if (showLoader) hiddeLoader();
+
   return (
     <HashRouter>
-      {/* <Loader /> */}
+      {showLoader && <Loader />}
       <img src={exitIcon} alt='boton' className='btn-exit' onClick={() => logout()} />
       <Routes>
         <Route path='/login' element={<Login />} />
