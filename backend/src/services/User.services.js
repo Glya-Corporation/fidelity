653a1fafd46839fdda36a5bcf8275fdcc749bf52
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2024-01-14 10:16:13 -0500

    update User.services.js

--- backend/src/services/User.services.js
+++ backend/src/services/User.services.js
@@ -66,7 +66,7 @@ class UserServices {
         attributes: {
           exclude: ['password', 'createdAt', 'updatedAt']
         },
-        order: [['id', 'DECS']],
+        order: [['id', 'DESC']],
         raw: true // Obtener resultados como objetos JSON sin instancias de Sequelize
       });
 
